4fa199587703c91e7a6ca994dad4accdf867bb23
jaxx035-dev <jaxx035@gmail.com>
2026-01-17 09:20:48 -0600

    Refactor word list handling and add random sampling

--- reader.py
+++ reader.py
@@ -1,1 +1,8 @@
 import json as jn
+import random as rm
+keywords = split(jn.keywords)
+wrd_1 = rm.sample(word_list1)
+wrd_2 = rm.sample(word_list2)
+word_list = wrd_1  and wrd_2
+if word_list.contains keywords:
+  

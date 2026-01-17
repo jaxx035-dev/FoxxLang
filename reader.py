bcbdb6629ad2856c077e5ec4c505cf95e3f54564
jaxx035-dev <jaxx035@gmail.com>
2026-01-17 09:34:20 -0600

    Fix split function usage for word lists

--- reader.py
+++ reader.py
@@ -1,8 +1,8 @@
 import json as jn
 import random as rm
 keywords1 = split(jn.keywords)
-word_list1 = jn.letters
-word_list2 = jn.LETTERS
+word_list1 = split(jn.letters)
+word_list2 = split(jn.LETTERS)
 wrd_1 = rm.sample(word_list1)
 wrd_2 = rm.sample(word_list2)
 word_list = wrd_1  and wrd_2
